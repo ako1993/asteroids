f7deaac6756d3d75677ebcb10a0a79fac09495be
ako1993 <andreasorourke@gmail.com>
2025-10-11 13:03:24 -0400

    Shots are working. No collision detection for them yet

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updateable, drawable)
     Asteroid.containers = (asteroids, updateable, drawable)
     AsteroidField.containers = (updateable)
+    Shot.containers = (shots, drawable, updateable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroid_field = AsteroidField()
